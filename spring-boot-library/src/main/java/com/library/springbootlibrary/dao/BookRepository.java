183dea034998dcd150191c300b598d8cb6effe77
Yosi7708 <yosi7708@gmail.com>
2023-08-27 23:42:46 +0300

    added findByTitle abd Pageable to the BookRepository

--- spring-boot-library/src/main/java/com/library/springbootlibrary/dao/BookRepository.java
+++ spring-boot-library/src/main/java/com/library/springbootlibrary/dao/BookRepository.java
@@ -1,7 +1,11 @@
 package com.library.springbootlibrary.dao;
 import com.library.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
-    //first commit
+    //now we can use this method to search for books by title and we can page the results
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
