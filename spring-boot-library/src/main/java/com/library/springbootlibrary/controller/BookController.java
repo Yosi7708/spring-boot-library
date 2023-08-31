87d63360fa7c1a8ba7f3e20ac2008c738e2fd58c
Yosi7708 <yosi7708@gmail.com>
2023-08-31 22:26:56 +0300

    implemented Spring boot Security with okta (and also downgraded the spring version to 2.7)

--- spring-boot-library/src/main/java/com/library/springbootlibrary/controller/BookController.java
+++ spring-boot-library/src/main/java/com/library/springbootlibrary/controller/BookController.java
@@ -2,10 +2,13 @@ package com.library.springbootlibrary.controller;
 
 import com.library.springbootlibrary.entity.Book;
 import com.library.springbootlibrary.service.BookService;
+import com.library.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+import java.util.List;
+
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
@@ -13,29 +16,40 @@ public class BookController {
     private BookService bookService;
 
     @Autowired
-    public BookController(BookService theBookService) {
-        this.bookService = theBookService;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
     @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount() {
-        String userEmail = "testuser@email.com";
-          return bookService.currentLoansCount(userEmail);
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean isCheckoutByUser(@RequestParam Long bookId) {
-        String userEmail = "testuser@email.com";
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token,
+                                      @RequestParam Long bookId) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
-        String userEmail = "testuser@email.com";
+    public Book checkoutBook (@RequestHeader(value = "Authorization") String token,
+                              @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
     }
 
 }
+
+
+
+
+
+
+
+
+
 
 
 
